e663711ed09a0b8cdecf767135c4a1e80aab46d5
Iamsaintm <p.leelayuthto@hotmail.com>
2023-08-10 09:19:48 +0700

    Update code

--- 02-do-while/01-findAverage/main.js
+++ 02-do-while/01-findAverage/main.js
@@ -3,12 +3,18 @@ let isEmpty;
 let isNan;
 let notInteger;
 sum = 0;
+ct = 0;
+avg = 0;
 do {
   number = prompt("Please your number") || "";
   isEmpty = number.trim() === "";
   isNan = isNaN(number);
   notInteger = Number(number) < 1;
-  if (!(isEmpty || isNan || notInteger)) sum += Number(number);
+  if (!(isEmpty || isNan || notInteger)) {
+    sum += Number(number);
+    ct += 1;
+  }
+  avg = +sum / +ct;
 } while (!(isEmpty || isNan || notInteger));
 
-alert(sum);
+alert(`sum is ${sum} : avg is ${avg}`);
